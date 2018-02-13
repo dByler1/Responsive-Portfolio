6bf972b4074346c39dbc7acbf2869b219e5e3ba8
Daniel Byler <danielbyler@Daniels-MacBook-Pro.local>
2018-02-13 18:21:30 -0500

    final commit before submitting

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,11 @@
-(function () {
-  
-    var navButton = document.getElementById("nav-menu-button"),
-        navUl = document.getElementsByClassName("nav-ul");
+
+  // var navButton = 
+  var navUl = document.getElementsByClassName("nav-ul");
        
     function toggleMobileMenu() {
       navUl[0].style.transition = "max-height 0.5s";
       navUl[0].classList.toggle("hide-ul");
     }
     
-    navButton.onclick = toggleMobileMenu;
-  }());
+    document.getElementById("nav-menu-button").onClick = toggleMobileMenu;
+
